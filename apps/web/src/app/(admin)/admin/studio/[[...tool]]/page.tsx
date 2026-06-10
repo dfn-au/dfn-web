3e31dd287ac699c5e1fd399db84849ccadd650d6
Andrew Harward <andrew@backpack.app>
2026-06-10 16:05:29 +1000

    feat: add admin shell to sanity studio

--- apps/web/src/app/(admin)/admin/studio/[[...tool]]/page.tsx
+++ apps/web/src/app/(admin)/admin/studio/[[...tool]]/page.tsx
@@ -3,13 +3,12 @@
  * All routes under `/admin/studio` are handled by this catch-all route.
  */
 
-import { NextStudio } from "next-sanity/studio";
-import config from "../../../../../../sanity.config";
+import { AdminStudioShell } from "./studio-shell";
 
 export const dynamic = "force-static";
 
 export { metadata, viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
-	return <NextStudio config={config} />;
+	return <AdminStudioShell />;
 }
